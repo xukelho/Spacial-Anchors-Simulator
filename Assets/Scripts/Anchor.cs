34b788c33dc9e8323f3fdf9cd5d41b13c43ebec6
XukeLho <xukelho@gmail.com>
2021-12-03 20:39:00 +0000

    Updated local position calculator

--- Assets/Scripts/Anchor.cs
+++ Assets/Scripts/Anchor.cs
@@ -1,50 +1,67 @@
 using System.Threading.Tasks;
 using UnityEngine;
 
-public class Anchor : MonoBehaviour
+namespace AnchorManagerSimulator
 {
-    #region Fields
-
-    public Vector3 GlobalPosition;
-    public Vector3 LocalPosition;
-    public Vector3 CameraPosition;
-    public Quaternion CameraRotation;
-    public Quaternion Rotation;
-
-    public AnchorManager AnchorManager;
-
-    #endregion Fields
-
-    #region Methods
-
-    /// <summary>
-    /// Recalculates the Anchor transform, relative to the camera, to keep it in place in the World.
-    /// </summary>
-    /// <returns></returns>
-    public async Task UpdateAnchor()
+    public class Anchor : MonoBehaviour
     {
-        //transform.localPosition = AnchorManager.ConvertWorldToLocalPoint(GlobalPosition);
-        CalculateNewLocalPosition();
-        transform.rotation = Quaternion.identity;
+        #region Fields
 
-        await Task.Yield();
+        /// <summary>
+        /// Global position of the object when was first instantiated. To be removed. Avoid usage.
+        /// </summary>
+        public Vector3 GlobalPosition;
+
+        /// <summary>
+        /// Local position of the object when was first instantiated.
+        /// </summary>
+        public Vector3 LocalPosition;
+
+        /// <summary>
+        /// Local rotation of the object when was first instantiated.
+        /// </summary>
+        public Quaternion LocalRotation;
+
+        /// <summary>
+        /// Global position of the camera when the object was first instantiated.
+        /// </summary>
+        public Vector3 CameraGlobalPosition;
+
+        /// <summary>
+        /// Global rotation of the camera when the object was first instantiated.
+        /// </summary>
+        public Quaternion CameraGlobalRotation;
+
+        /// <summary>
+        /// Anchor Manager / Main Camera reference
+        /// </summary>
+        public AnchorManager AnchorManager;
+
+        #endregion Fields
+
+        #region Methods
+
+        /// <summary>
+        /// Recalculates the Anchor transform, relative to the camera, to keep it in place in the World.
+        /// </summary>
+        /// <returns></returns>
+        public async Task UpdateAnchor()
+        {
+            //transform.localPosition = AnchorManager.ConvertWorldToLocalPoint(GlobalPosition);
+            //transform.rotation = Quaternion.identity;
+
+            CalculateNewLocalPosition();
+
+            await Task.Yield();
+        }
+
+        void CalculateNewLocalPosition()
+        {
+            Vector3 cameraMovedOffset = AnchorManager.transform.position - CameraGlobalPosition;
+            Vector3 newLocalPosition = cameraMovedOffset + LocalPosition;
+            transform.localPosition = newLocalPosition;
+        }
+
+        #endregion Methods
     }
-
-    void CalculateNewLocalPosition()
-    {
-        Matrix4x4 matrixExample = Matrix4x4.identity;
-
-        matrixExample.SetTRS(LocalPosition, CameraRotation, Vector3.one);
-
-        Vector3 localSpacePosition = matrixExample.inverse.MultiplyPoint3x4(GlobalPosition);
-        transform.localPosition = localSpacePosition;
-
-        //-----------
-        //Quaternion newRotation = CameraRotation * Quaternion.Inverse(AnchorManager.transform.rotation);
-        //Vector3 rotatedPoint = newRotation * LocalPosition;
-
-        //transform.localPosition = rotatedPoint;
-    }
-
-    #endregion Methods
 }
--- Assets/Scripts/AnchorManager.cs
+++ Assets/Scripts/AnchorManager.cs
@@ -2,89 +2,97 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 
-public class AnchorManager : MonoBehaviour
+namespace AnchorManagerSimulator
 {
-    #region Fields
-
-    [Tooltip("How often, in seconds, to recalculate all anchors positions.")]
-    public float AnchorUpdateInterval = .1f;
-
-    [Header("Prefab References")]
-    public Anchor PrefabAnchor;
-
-    public Vector3 StartingWorldPosition { get; private set; }
-
-    Camera _camera;
-
-    List<Anchor> _anchors = new List<Anchor>();
-
-    #endregion Fields
-
-    #region Unity
-
-    void Awake()
+    public class AnchorManager : MonoBehaviour
     {
-        _camera = GetComponent<Camera>();
-        StartingWorldPosition = transform.position;
-    }
+        #region Fields
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-            SpawnAnchor();
+        [Tooltip("How often, in seconds, to recalculate all anchors positions.")]
+        public float AnchorUpdateInterval = .1f;
 
-        UpdateAnchors();
-    }
+        [Header("Prefab References")]
+        public Anchor PrefabAnchor;
 
-    #endregion Unity
+        public Vector3 StartingWorldPosition { get; private set; }
 
-    #region Methods
+        Camera _camera;
 
-    //TODO
-    public Vector3 ConvertWorldToLocalPoint(Vector3 point)
-    {
-        Vector3 localPoint = _camera.transform.InverseTransformPoint(point);
-        return localPoint;
-    }
+        List<Anchor> _anchors = new List<Anchor>();
 
-    /// <summary>
-    /// Spawns an Anchor in the intersection of a ray from the center of the screen, to the first collider hit.
-    /// </summary>
-    void SpawnAnchor()
-    {
-        Ray ray = _camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        Vector3 _lastFrameCameraPosition;
+        Quaternion _lastFrameCameraRotation;
 
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        #endregion Fields
+
+        #region Unity
+
+        void Awake()
         {
-            Anchor anchor = Instantiate(PrefabAnchor);
-            _anchors.Add(anchor);
-
-            anchor.transform.parent = _camera.transform;
-            anchor.transform.localPosition = _camera.transform.InverseTransformPoint(hit.point);
-            anchor.GlobalPosition = hit.point;
-            anchor.CameraPosition = transform.position;
-            anchor.CameraRotation = transform.rotation;
-            anchor.LocalPosition = anchor.transform.localPosition;
-            anchor.AnchorManager = this;
-
-            anchor.name = "Anchor " + _anchors.Count;
+            _camera = GetComponent<Camera>();
+            StartingWorldPosition = transform.position;
         }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+                SpawnAnchor();
+
+            if (_lastFrameCameraPosition == transform.position && _lastFrameCameraRotation == transform.rotation)
+                return;
+
+            UpdateAnchors();
+
+            _lastFrameCameraPosition = transform.position;
+            _lastFrameCameraRotation = transform.rotation;
+        }
+
+        #endregion Unity
+
+        #region Methods
+
+        /// <summary>
+        /// Spawns an Anchor in the intersection of a ray from the center of the screen, to the first collider hit.
+        /// </summary>
+        void SpawnAnchor()
+        {
+            Ray ray = _camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Anchor anchor = Instantiate(PrefabAnchor);
+                _anchors.Add(anchor);
+
+                anchor.transform.parent = _camera.transform;
+                anchor.transform.position = hit.point;
+                anchor.transform.LookAt(new Vector3(transform.position.x, anchor.transform.position.y, transform.position.z));
+
+                anchor.GlobalPosition = hit.point;
+                anchor.CameraGlobalPosition = transform.position;
+                anchor.CameraGlobalRotation = transform.rotation;
+                anchor.LocalPosition = anchor.transform.localPosition;
+                anchor.LocalRotation = anchor.transform.localRotation;
+                anchor.AnchorManager = this;
+
+                anchor.name = "Anchor " + _anchors.Count;
+            }
+        }
+
+        /// <summary>
+        /// Controls the update of all anchors.
+        /// </summary>
+        async void UpdateAnchors()
+        {
+            await Task.Yield();
+
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < _anchors.Count; i++)
+                tasks.Add(_anchors[i].UpdateAnchor());
+
+            await Task.WhenAll(tasks);
+        }
+
+        #endregion Methods
     }
-
-    /// <summary>
-    /// Controls the update of all anchors.
-    /// </summary>
-    async void UpdateAnchors()
-    {
-        await Task.Yield();
-
-        var tasks = new List<Task>();
-
-        for (int i = 0; i < _anchors.Count; i++)
-            tasks.Add(_anchors[i].UpdateAnchor());
-
-        await Task.WhenAll(tasks);
-    }
-
-    #endregion Methods
 }
